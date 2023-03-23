1829a8c03a2941dfb89cfb0bd89dd8db3160cbb7
Jakub Nowak <jakub.nowak@zst.kolbuszowa.pl>
2023-03-23 12:11:13 +0100

    string

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // napisz program łączący 2 słowa i wypisujący na końcu
+using namespace std;
+int main() {
+    string imie="Jakub ";
+    string nazwisko="Nowak";
+    cout << imie + nazwisko;
+    return 0;
+}
